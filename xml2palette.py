d94f062b289b657e7e1f28bb7d0c7882300a164c
james-strauss-uwa <james.strauss@uwa.edu.au>
2020-07-28 13:38:00 +0800

    Traverse the XML file and create a palette node based on the data.

--- xml2palette.py
+++ xml2palette.py
@@ -47,42 +47,98 @@ def create_port(name):
     }
 
 
-def create_fields(category):
-    if category == "DynlibApp":
-        return [
-            {
-                "text": "Execution time",
-                "name": "execution_time",
-                "value": "5"
-            }, {
-                "text": "Num CPUs",
-                "name": "num_cpus",
-                "value": "1"
-            }, {
-                "text": "Group start",
-                "name": "group_start",
-                "value": "0"
-            }, {
-                "text": "Library path",
-                "name": "libpath",
-                "value": ""
-            }]
-    else:
-        print("Unknown category: " + category + ". Unable to create fields.")
-        return []
+def add_required_fields_for_category(fields, category):
+    # if category == "DynlibApp":
+    #     return [
+    #         {
+    #             "text": "Execution time",
+    #             "name": "execution_time",
+    #             "value": "5"
+    #         }, {
+    #             "text": "Num CPUs",
+    #             "name": "num_cpus",
+    #             "value": "1"
+    #         }, {
+    #             "text": "Group start",
+    #             "name": "group_start",
+    #             "value": "0"
+    #         }, {
+    #             "text": "Library path",
+    #             "name": "libpath",
+    #             "value": ""
+    #         }]
+    # else:
+    #     print("Unknown category: " + category + ". Unable to create fields.")
+    #     return []
+    pass
+
+
+def create_field(text, name, value, description):
+    return {
+        "text": text,
+        "name": name,
+        "value": value,
+        "description": description
+    }
 
 
 # NOTE: color, x, y, width, height are not specified in palette node, they will be set by the EAGLE importer
-def create_palette_node(text, description, category, categoryType, inputPorts, outputPorts):
-    newInputPorts = []
-    newOutputPorts = []
+def create_palette_node_from_params(params):
+    gitrepo = ""
+    version = ""
+    text = ""
+    description = ""
+    category = ""
+    categoryType = ""
+    inputPorts = []
+    outputPorts = []
+    inputLocalPorts = []
+    outputLocalPorts = []
+    fields = []
 
-    for inputPort in inputPorts:
-        newInputPorts.append(create_port(inputPort))
+    # process the params
+    for param in params:
+        key = param['key']
+        direction = param['direction']
+        value = param['value']
 
-    for outputPort in outputPorts:
-        newOutputPorts.append(create_port(outputPort))
+        if key == "category":
+            category = value
+        elif key == "gitrepo":
+            gitrepo = value
+        elif key == "version":
+            version = value
+        elif key.startswith("param/"):
+            # parse the param key into name, type etc
+            (param, name, default_value, type) = key.split("/")
+            # add a field
+            fields.append(create_field(name + " (" + type + ")", name, default_value, value))
+            pass
+        elif key.startswith("port/") or key.startswith("local-port/"):
+            # parse the port into data
+            (port, name) = key.split("/")
+            # add a port
+            if port == "port":
+                if direction == "in":
+                    inputPorts.append(create_port(name))
+                elif direction == "out":
+                    outputPorts.append(create_port(name))
+                else:
+                    print("ERROR: Unknown port direction: " + direction)
+            else:
+                if direction == "in":
+                    inputLocalPorts.append(create_port(name))
+                elif direction == "out":
+                    outputLocalPorts.append(create_port(name))
+                else:
+                    print("ERROR: Unknown local-port direction: " + direction)
+        else:
+            print("ERROR: unknown param: " + key)
 
+    # add extra fields that must be included for the category
+    add_required_fields_for_category(fields, category)
+
+    # create and return the node
     return {
         "category": category,
         "categoryType": categoryType,
@@ -106,13 +162,13 @@ def create_palette_node(text, description, category, categoryType, inputPorts, o
         "inputApplicationType": "Unknown",
         "outputApplicationType": "Unknown",
         "exitApplicationType": "None",
-        "inputPorts": newInputPorts,
-        "outputPorts": newOutputPorts,
-        "inputLocalPorts": [],
-        "outputLocalPorts": [],
+        "inputPorts": inputPorts,
+        "outputPorts": outputPorts,
+        "inputLocalPorts": inputLocalPorts,
+        "outputLocalPorts": outputLocalPorts,
         "inputAppFields": [],
         "outputAppFields": [],
-        "fields": create_fields(category)
+        "fields": fields
     }
 
 
@@ -135,6 +191,69 @@ def write_palette_json(outputfile, nodes):
         json.dump(palette, outfile, indent=4)
 
 
+def process_compounddef(compounddef):
+    #print("compounddef: " + compounddef.attrib['id'])
+    result = []
+
+    # get child of componddef called "detaileddescription"
+    detaileddescription = None
+    for child in compounddef:
+        if child.tag == "detaileddescription":
+            detaileddescription = child
+            break
+
+    # check that detailed description was found
+    if detaileddescription is None:
+        #print("No detaileddescription")
+        return result
+
+    #print("detaileddescription" + str(detaileddescription))
+
+    # search children of detaileddescription node for a para node with "simplesect" children, who have "title" children with text "EAGLE_START" and "EAGLE_END"
+    para = None
+    for ddchild in detaileddescription:
+        if ddchild.tag == "para":
+            for pchild in ddchild:
+                if pchild.tag == "simplesect":
+                    para = ddchild
+                    break
+
+    # check that we found the correct para
+    if para is None:
+        #print("No para")
+        return result
+
+    # find parameterlist child of para
+    parameterlist = None
+    for pchild in para:
+        #print("pchild tag " + pchild.tag)
+        if pchild.tag == "parameterlist":
+            parameterlist = pchild
+            break
+
+    # check that we found a parameterlist
+    if parameterlist is None:
+        #print("No parameterlist")
+        return result
+
+    # read the parameters from the parameter list
+    for parameteritem in parameterlist:
+        #print("process parameteritem")
+        key = None
+        direction = None
+        value = None
+        for pichild in parameteritem:
+            if pichild.tag == "parameternamelist":
+                key = pichild[0].text.strip()
+                direction = pichild[0].attrib.get("direction", "").strip()
+            elif pichild.tag == "parameterdescription":
+                value = pichild[0].text.strip()
+        #print("key: " + key + " direction: " + str(direction) + " value: " + value)
+        result.append({"key":key,"direction":direction,"value":value})
+
+    return result
+
+
 if __name__ == "__main__":
     (inputfile, outputfile) = get_filenames_from_command_line(sys.argv[1:])
 
@@ -145,16 +264,25 @@ if __name__ == "__main__":
     nodes = []
 
     # load the input xml file
-    #tree = ET.parse(inputfile)
-    #xml_root = tree.getroot()
+    tree = ET.parse(inputfile)
+    xml_root = tree.getroot()
     #print(xml_root.tag)
 
-    # debug : add a sample node
-    n0 = create_palette_node("text", "description", "DynlibApp", "Application", ["event"], ["event"])
-    n1 = create_palette_node("text", "description", "DynlibApp", "Application", ["event"], ["event"])
+    for compounddef in xml_root:
+        params = process_compounddef(compounddef)
+        if len(params) > 0:
+            #print("params: " + str(params))
 
-    nodes.append(n0)
-    nodes.append(n1)
+            # create a node
+            n = create_palette_node_from_params(params)
+            nodes.append(n)
+
+    # debug : add a sample node
+    #n0 = create_palette_node("text", "description", "DynlibApp", "Application", ["event"], ["event"])
+    #n1 = create_palette_node("text", "description", "DynlibApp", "Application", ["event"], ["event"])
+
+    #nodes.append(n0)
+    #nodes.append(n1)
 
     # debug
     #print("add node: " + str(n0['key']))
